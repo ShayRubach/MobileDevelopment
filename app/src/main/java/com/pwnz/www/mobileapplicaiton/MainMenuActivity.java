3d0c945415c1c6540f339e45a144e8140b9ab612
ShayRubach <shayrubach@gmail.com>
2018-04-28 05:06:49 +0200

    added the click listener to calc activity. can now navigate between activities.

--- app/src/main/java/com/pwnz/www/mobileapplicaiton/MainMenuActivity.java
+++ app/src/main/java/com/pwnz/www/mobileapplicaiton/MainMenuActivity.java
@@ -1,5 +1,6 @@
 package com.pwnz.www.mobileapplicaiton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -21,6 +22,10 @@ public class MainMenuActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private TaskAdapter mAdapter;
 
+    public static final String ACT_CALCULATOR = "Simple Calculator";
+    public static final String ACT_BIRTHDAYS = "Birthday List";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +41,7 @@ public class MainMenuActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mAdapter);
 
 
-        mTaskList.add(new Task("Simple Calculator ", "#1", "1/1/18"));
+        mTaskList.add(new Task("Simple Calculator", "#1", "1/1/18"));
         mTaskList.add(new Task("Birthday List", "#2", "1/1/18"));
         mTaskList.add(new Task("Future Task", "#3", "1/1/18"));
 
@@ -44,13 +49,22 @@ public class MainMenuActivity extends AppCompatActivity {
         mRecyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), mRecyclerView, new RecyclerTouchListener.ClickListener() {
             @Override
             public void onClick(View view, int position) {
-                Task task = mTaskList.get(position);
-                Toast.makeText(getApplicationContext(), task.getTitle() + " is selected!", Toast.LENGTH_SHORT).show();
+                //Task task = mTaskList.get(position);
+                //Toast.makeText(getApplicationContext(), task.getTitle() + " is selected!", Toast.LENGTH_SHORT).show();
+                switch(mTaskList.get(position).getTitle()){
+                    case ACT_CALCULATOR:
+                       startActivity(new Intent(MainMenuActivity.this, CalculatorActivity.class));
+                        break;
+                    case ACT_BIRTHDAYS:
+                        break;
+                    default:
+                        break;
+                }
             }
 
             @Override
             public void onLongClick(View view, int position) {
-                
+
             }
         }));
 
