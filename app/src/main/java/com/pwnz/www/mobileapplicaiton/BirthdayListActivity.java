ef4fdd014ed35682c54c4b76d395a69ae4801b24
ShayRubach <shayrubach@gmail.com>
2018-05-02 13:18:15 +0200

    added a dialog for adding a bday

--- app/src/main/java/com/pwnz/www/mobileapplicaiton/BirthdayListActivity.java
+++ app/src/main/java/com/pwnz/www/mobileapplicaiton/BirthdayListActivity.java
@@ -1,10 +1,16 @@
 package com.pwnz.www.mobileapplicaiton;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.pwnz.www.mobileapplicaiton.controller.BirthdayAdapter;
 import com.pwnz.www.mobileapplicaiton.controller.TaskAdapter;
@@ -26,6 +32,10 @@ public class BirthdayListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_birthday_list);
+        FloatingActionButton btnAddBday = findViewById(R.id.btnAddBirthday);
+
+
+
 
         mBirthdayAdapter = new BirthdayAdapter(mBirthdayList);
 
@@ -49,6 +59,44 @@ public class BirthdayListActivity extends AppCompatActivity {
         mBirthdayList.add(new Birthday("14/2/1990", "Brownie Shytles", R.drawable.avatar11));
         mBirthdayList.add(new Birthday("14/2/1990", "Willie Stroker", R.drawable.avatar12));
         mBirthdayList.add(new Birthday("14/2/1990", "Candy Cummings", R.drawable.avatar13));
+
+
+
+        btnAddBday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(BirthdayListActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_add_birthday, null);
+
+                final EditText mPersonName = mView.findViewById(R.id.dialogPersonName);
+                final EditText mDateDay = mView.findViewById(R.id.dialogDateDay);
+                final EditText mDateMonth = mView.findViewById(R.id.dialogDateMonth);
+                final EditText mDateYear = mView.findViewById(R.id.dialogDateYear);
+                final Button mBtnDone = mView.findViewById(R.id.btnAddDone);
+
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+
+                mBtnDone.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(!mPersonName.getText().toString().isEmpty() &&
+                                !mDateDay.getText().toString().isEmpty() &&
+                                !mDateMonth.getText().toString().isEmpty() &&
+                                !mDateYear.getText().toString().isEmpty()){
+
+                            Toast.makeText(BirthdayListActivity.this, R.string.success, Toast.LENGTH_SHORT).show();
+                            dialog.hide();
+                        }
+                        else {
+                            Toast.makeText(BirthdayListActivity.this, R.string.emptyFields, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
 
     }
 }
